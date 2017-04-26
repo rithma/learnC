2d2d414a2c21b3997853ffc5e6112ae0f92d8a71
rithma <rithers@gmail.com>
2017-04-25 18:00:14 -0700

    Create libex30.c

---
+++ ex30WORKS/libex30.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "dbg.h"
+
+
+int square (const char *x)
+{
+    int ain;
+    ain = atoi(x);
+    int p;
+    p = ain * ain;
+    printf("heres your fucking square: %d\n", p);
+    //return (p);
+    return (p);
+
+}
+
+int print_a_message(const char *msg)
+{
+    printf("A STRING: %s\n", msg);
+    
+    return 0;
+}
+
+
+int uppercase(const char *msg)
+{
+    int i = 0;
+        
+    //BUG: \0 termination problems
+    for(i = 0; msg[i] != '\0'; i++) {
+        printf("%c", toupper(msg[i]));
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
+int lowercase(const char *msg)
+{
+    int i = 0;
+    
+    //BUG: \0 termination problems
+    for(i = 0; msg[i] != '\0'; i++) {
+        printf("%c", tolower(msg[i]));
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
+
+int fail_on_purpose(const char *msg)
+{
+    return 1;
+}
